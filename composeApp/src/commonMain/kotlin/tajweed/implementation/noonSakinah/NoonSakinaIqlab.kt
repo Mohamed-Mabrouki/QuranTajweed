e83c3af16b45a91678fee8a1de6a527d54e493c3
Mabrouki Mohamed <mabroukimohamed2004@gmail.com>
2024-08-01 14:25:04 +0100

    save

--- composeApp/src/commonMain/kotlin/tajweed/implementation/noonSakinah/NoonSakinaIqlab.kt
+++ composeApp/src/commonMain/kotlin/tajweed/implementation/noonSakinah/NoonSakinaIqlab.kt
@@ -1,4 +1,17 @@
 package tajweed.implementation.noonSakinah
 
-class NoonSakinaIqlab {
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import tajweed.abstraction.TajweedRule
+
+class NoonSakinaIqlab(
+    override val color: Color = Color.Magenta,
+    override val characterVariation: List<Char> = listOf('ن'),
+    override val followingNormalCharacters: List<Char>,
+    override val followingChaklCharacters: List<Char>,
+    override val numberOfCharactersToColor: Int,
+) : TajweedRule {
+    override fun highlightQuranText(quranText: AnnotatedString): AnnotatedString {
+        TODO("Not yet implemented")
+    }
 }
